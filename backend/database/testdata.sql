c0113f6f75e0166ee13c0fa8c9bda59cc17ff1be
Quinnn Hebert <hebert.42@osu.edu>
2019-04-09 15:24:06 -0400

    update testdata.sql

--- backend/database/testdata.sql
+++ backend/database/testdata.sql
@@ -1,15 +1,15 @@
 BEGIN TRANSACTION;   
 
-INSERT INTO trainer (first_name, last_name, username, password, salt)
-VALUES ('Lance', 'Armstrong', 'LanceArm', 'roidcheater', 'salt1');
+INSERT INTO users(first_name, last_name, username, password, salt, role)
+VALUES ('Lance', 'Armstrong', 'LanceArm', 'roidcheater', 'salt1', 'trainer');
 
-INSERT INTO client(first_name, last_name, username, password, salt)
-VALUES ('Terry', 'Reily', 'TerryR1', 'catLuvnum1', 'salt2');
+INSERT INTO users(first_name, last_name, username, password, salt, role)
+VALUES ('Terry', 'Reily', 'TerryR1', 'catLuvnum1', 'salt2', 'client');
 
-INSERT INTO trainer_profile(trainerId, is_public, price_per_hour, rating, philosphy, bio, city, state, certifications)
-VALUES((SELECT trainerId FROM trainer WHERE username = 'LanceArm'), 'false', '100', '3', 'I like to bike', 'former Tour De France winner', 'Austin', 'TX', 'retacted');
+INSERT INTO trainer_profile(user_id, is_public, price_per_hour, rating, philosphy, bio, city, state, certifications)
+VALUES((SELECT user_id FROM users WHERE username = 'LanceArm'), 'false', '100', '3', 'I like to bike', 'former Tour De France winner', 'Austin', 'TX', 'retacted');
 
 INSERT INTO client_list(trainerId, clientId)
-VALUES ((SELECT trainerId FROM trainer WHERE username = 'LanceArm'), (SELECT clientId FROM client WHERE username = 'TerryR1'));
+VALUES ((SELECT user_id FROM users WHERE username = 'LanceArm'), (SELECT user_id FROM users WHERE username = 'TerryR1'));
 
 COMMIT TRANSACTION;
