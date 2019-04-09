67b6473bbd4f1f94269b9d35088300ad1cf4f716
Quinnn Hebert <hebert.42@osu.edu>
2019-04-09 15:45:17 -0400

    .

--- backend/database/testdata.sql
+++ backend/database/testdata.sql
@@ -1,10 +1,10 @@
 BEGIN TRANSACTION;   
 
 INSERT INTO users(first_name, last_name, username, password, salt, role)
-VALUES ('Lance', 'Armstrong', 'LanceArm', 'roidcheater', 'salt1', 'trainer');
+VALUES ('Lance', 'Armstrong', 'LanceArm', 'roidcheater', 'salt1', 'Trainer');
 
 INSERT INTO users(first_name, last_name, username, password, salt, role)
-VALUES ('Terry', 'Reily', 'TerryR1', 'catLuvnum1', 'salt2', 'client');
+VALUES ('Terry', 'Reily', 'TerryR1', 'catLuvnum1', 'salt2', 'Client');
 
 INSERT INTO trainer_profile(user_id, is_public, price_per_hour, rating, philosphy, bio, city, state, certifications)
 VALUES((SELECT user_id FROM users WHERE username = 'LanceArm'), 'false', '100', '3', 'I like to bike', 'former Tour De France winner', 'Austin', 'TX', 'retacted');
