5fcbb75fb0db2dce0f5b2ae738205ff23a4d99e8
Quinnn Hebert <hebert.42@osu.edu>
2019-04-11 13:48:50 -0400

    TrainerProfileDao updated and search for Trainer Profile Working

--- backend/database/testdata.sql
+++ backend/database/testdata.sql
@@ -6,8 +6,8 @@ VALUES ('Lance', 'Armstrong', 'LanceArm', 'roidcheater', 'salt1', 'Trainer');
 INSERT INTO users(first_name, last_name, username, password, salt, role)
 VALUES ('Terry', 'Reily', 'TerryR1', 'catLuvnum1', 'salt2', 'Client');
 
-INSERT INTO trainer_profile(user_id, is_public, price_per_hour, rating, philosphy, bio, city, state, certifications)
-VALUES((SELECT user_id FROM users WHERE username = 'LanceArm'), 'false', '100', '3', 'I like to bike', 'former Tour De France winner', 'Austin', 'TX', 'retacted');
+INSERT INTO trainer_profile(user_id, is_public, first_name, last_name, price_per_hour, rating, philosphy, bio, city, state, certifications)
+VALUES((SELECT user_id FROM users WHERE username = 'LanceArm'), 'false', 'Lance', 'Armstrong', '100', '3', 'I like to bike', 'former Tour De France winner', 'Austin', 'TX', 'retacted');
 
 INSERT INTO client_list(trainerId, clientId)
 VALUES ((SELECT user_id FROM users WHERE username = 'LanceArm'), (SELECT user_id FROM users WHERE username = 'TerryR1'));
