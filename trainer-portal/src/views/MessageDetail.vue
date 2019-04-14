fc601cc145f23f036769f0f7e16847dae2488977
Mike Stagney <mikestagney@yahoo.com>
2019-04-13 22:01:58 -0400

    message detail

--- trainer-portal/src/views/MessageDetail.vue
+++ trainer-portal/src/views/MessageDetail.vue
@@ -4,18 +4,19 @@
     <div class="messageContainer pb-4 pl-4 pr-4 pt-2">
       <div class="row messageHeader text-light mb-3 p-5 shadow">
                 <div class="col">
-                <h2 id="test">Message</h2>
+                <h2 id="test">Private Message</h2>
                 </div>
        </div>
       <div class="subject text-center">
-      <h3>"{{ this.message.subject }}"</h3>
+      <h3>Subject: {{ message.subject }}</h3>
       </div>
-      <p><span class="orangeText">From:</span> {{ message.senderId  + ' ' + message.receipientId }}, {{message.role}}</p>
-      <p><span class="orangeText">Sent:</span> {{ message.postDate  }}</p>
+      <p><span class="orangeText">From:</span> {{ message.senderId  + ' ' + message.receipientId }}, {{message.role}}
+      <span class="orangeText">Sent on:</span> {{ message.postDate  }}</p>
       <div class="message p-3">
       <p> {{ message.message }} </p>
       </div>
-
+        <button v-on:click="replyToSender">Reply</button>
+        <button v-on:click="deleteMessage">Delete Message</button>
 
     </div>
   </div>
@@ -33,12 +34,12 @@ export default {
     return {
       message: 
         {
-        messageId: 0,
-        senderId: 0,
-        receipientId: 0,
-        postDate: '',
-        subject: '',
-        message: '',
+        messageId: 1,
+        senderId: 1,
+        receipientId: 5,
+        postDate: '2/12/19',
+        subject: 'test',
+        message: 'this is the body of the message',
         unread: false,
         senderDelete: false,
         recipientDelete: false
@@ -46,7 +47,9 @@ export default {
     };
   },
   created() {
-    const messageIdPuller = this.$route.params.MessageID;
+    /*
+   const messageIdPuller = this.$route.params.MessageID;
+   
     fetch(`${process.env.VUE_APP_REMOTE_API}/inbox/${messageIdPuller}`, {
       method: 'GET',
         headers: new Headers ({
@@ -61,6 +64,7 @@ export default {
           this.message = message;
       })
       .catch((err) => console.error(err));
+      */
   },
   methods: {
     deleteMessage() {
