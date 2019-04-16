12978daa95d6ad3911128b8cae57008d051c0169
Quinnn Hebert <hebert.42@osu.edu>
2019-04-16 15:16:08 -0400

    .

--- trainer-portal/src/views/WorkoutPlan.vue
+++ trainer-portal/src/views/WorkoutPlan.vue
@@ -40,7 +40,7 @@ export default {
 
     },
   created() {
-      fetch(`${process.env.VUE_APP_REMOTE_API}workout/${this.UserID}`, {
+      fetch(`${process.env.VUE_APP_REMOTE_API}workout/1`, {
       method: 'GET',
         headers: new Headers ({
           Authorization: 'Bearer ' + auth.getToken(),
