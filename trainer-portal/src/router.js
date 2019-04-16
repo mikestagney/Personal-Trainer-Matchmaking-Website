1fc77c8ffa62e7651f00a4751af0ca400f9ec29a
Quinnn Hebert <hebert.42@osu.edu>
2019-04-16 15:03:19 -0400

    .

--- trainer-portal/src/router.js
+++ trainer-portal/src/router.js
@@ -13,6 +13,8 @@ import ClientList from '@/views/ClientList';
 import AddTrainer from '@/views/AddTrainer';
 import TrainerList from '@/views/TrainerList';
 import EditTrainerProfile from '@/views/EditTrainerProfile';
+import WorkoutPlan from '@/views/WorkoutPlan';
+import WorkoutPlans from '@/views/WorkoutPlans';
 
 Vue.use(Router);
 
@@ -79,6 +81,21 @@ const router = new Router({
         path: '/trainer/profile/:TrainerID/edit',
         name: 'editTrainerProfile',
         component: EditTrainerProfile
+      },
+      {
+        path: '/workout/:UserID',
+        name: 'listWorkoutPlans',
+        component: WorkoutPlan
+      },
+      {
+        path: '/workout/:UserID',
+        name: 'listWorkoutPlans',
+        component: WorkoutPlans
+      },
+      {
+        path: '/workout/:WorkoutPlanID',
+        name: 'workoutPlan',
+        component: WorkoutPlan
       },
     ],
   });
---
+++ trainer-portal/src/views/WorkoutPlan.vue
@@ -0,0 +1,65 @@
+<template>
+<default-layout>
+    <div class="trainer-list container">
+        <table class="table table-striped mt-4">
+            <thead class="thead thead-light">
+            <tr>
+            <th scope="col">Days Of Week (Sun,Mon,Tues,Wed,Thurs,Fri,Sat)</th>
+            <th scope="col">Work Out: </th>
+            <th scope="col">Plan: </th>
+            </tr>
+            </thead>
+            <tbody>
+                <tr >
+                    <td>{{ workout.daysOfWeek }}</td>
+                    <td>{{trainer.title}}</td>
+                    <td>{{trainer.body}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    
+</default-layout>
+</template>
+
+<script>
+import DefaultLayout from '@/layouts/DefaultLayout';
+import auth from '../auth';
+
+export default {
+    components: {
+        DefaultLayout
+    },
+    data(){
+        return{
+            UserID: this.$route.params.UserID,
+            workoutPlan: ''
+        }
+    },
+    methods: {
+
+    },
+  created() {
+      fetch(`${process.env.VUE_APP_REMOTE_API}workout/${this.UserID}`, {
+      method: 'GET',
+        headers: new Headers ({
+          Authorization: 'Bearer ' + auth.getToken(),
+        }),
+        credentials: 'same-origin',
+      }) 
+        .then((response) => {
+            return response.json();
+        })
+        .then((json) => {
+            this.workoutPlan = json;
+        })
+        .catch((err) => console.error(err));
+    }
+ 
+}
+</script>
+
+<style>
+
+
+</style>
