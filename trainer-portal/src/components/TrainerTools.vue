167167dcc1b7e5e27a094601f1be8130de0cd5e1
Quinnn Hebert <hebert.42@osu.edu>
2019-04-17 23:22:08 -0400

    createWorkOutPlan is working and just needs some formatting/css

--- trainer-portal/src/components/TrainerTools.vue
+++ trainer-portal/src/components/TrainerTools.vue
@@ -11,7 +11,7 @@
                 <router-link to="/clientList" exact>
                     <button class="text-light btn-lg exclusiveButton">View Clients</button>
                 </router-link>
-                <router-link to="" exact>
+                <router-link v-bind:to="{ name: 'create-workout-plan', params: { ClientID: ClientID}}" exact>
                     <button class="text-light btn-lg exclusiveButton ml-2">Create Workouts</button>
                 </router-link>
                 <router-link to="" exact>
